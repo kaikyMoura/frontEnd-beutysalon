c3de6aa44a96ea238baf3960d3ca9aa587e70aa1
kaiky de Moura <kaikymoura972@gmail.com>
2024-10-04 21:27:41 -0300

    Alterando condicional de navegação da rota

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -13,10 +13,12 @@ const PageLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
     const { setLoading } = useLoadingContext()
 
     useEffect(() => {
-        if (router.pathname !== '/home' && router.pathname === '/' || router.pathname === '/_error') {
-            router.replace('/home');
-            setPageTitle('Início')
+        if (router.pathname === '/' || router.pathname === '/_error') {
+            if (router.pathname !== '/home') {
+                router.push('/home');
+            }
         }
+        
     }, [router, setLoading, setPageTitle]);
 
     useEffect(() => {
